9adecfc7497f91d4f503459276bd5ee8674c5da9
geovani-nascto <104122527+geovani-nascto@users.noreply.github.com>
2023-02-11 01:19:09 -0300

    pre-css

---
+++ assets/js/script.js
@@ -0,0 +1,96 @@
+class Form{
+    method = 'GET';
+    items = [];
+
+    constructor(container, method, action){
+        this.container = document.querySelector(container);
+        this.method = method;
+        this.action = action;
+    }
+
+    addItem(item){
+        this.items.push(item);
+    }
+
+    displayOn(){
+        let formElement = document.createElement('form');
+        formElement.setAttribute('method', this.method);
+        formElement.setAttribute('action', this.action);
+
+        for(let i in this.items){
+            this.items[i].displayOn(formElement);
+        }
+
+        this.container.appendChild(formElement);
+    }
+}
+
+class Input{
+    required = false;
+    _type = 'text'
+    
+    constructor(name, label, className){
+        this.name = name;
+        this.label = label;
+        this.className = className;
+    }
+
+    get type(){
+        return this._type;
+    }
+
+    set type(type){
+        if(['text', 'email', 'password', 'tel', 'submit'].includes(type)){
+            this._type = type;
+        } else{
+            throw new Error(`O input de tipo ${type} não existe.`);
+        }
+    }
+
+    displayOn(formElement){
+        let inputField = document.createElement('input');
+        inputField.name = this.name;
+        inputField.placeholder = this.label;
+        inputField.type = this.type;
+        inputField.required = this.required;
+        inputField.className = this.className;
+        formElement.appendChild(inputField);
+    }
+}
+
+class Button extends Input{
+
+    constructor(label, className){
+        super('', label, className);
+        this.type = 'submit';
+    }
+
+    displayOn(formElement){
+        let buttonInput = document.createElement('input');
+        buttonInput.type = this.type;
+        buttonInput.value = this.label;
+        buttonInput.className = this.className;
+        formElement.appendChild(buttonInput);
+    }
+}
+
+let form = new Form('.formArea', 'POST', '#');
+
+let email = new Input('email', 'Digite seu e-mail', 'inputField');
+email.type = 'email';
+email.required = true;
+form.addItem(email);
+
+let password = new Input('password', 'Digite sua senha', 'inputField');
+password.type = 'password';
+password.required = true;
+form.addItem(password);
+
+let tel = new Input('tel', 'Digite seu número de telefone', 'inputField');
+tel.type = 'tel';
+form.addItem(tel);
+
+let button = new Button('Enviar', 'buttonInput');
+form.addItem(button);
+
+form.displayOn();
